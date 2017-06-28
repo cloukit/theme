0e23846f6622d271b69c23f922c8839475c6f24b
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-06-28 22:31:19 +0200

    1.2.8 add prefixer registration

--- src/components/theme.service.ts
+++ src/components/theme.service.ts
@@ -5,25 +5,36 @@
  */
 import { Injectable } from '@angular/core';
 import { CloukitComponentTheme, RegisteredTheme } from './theme.model';
+import { CloukitStatefulAndModifierAwareElementThemeStyleDefinition } from './theme.model';
 
 @Injectable()
 export class CloukitThemeService {
 
   private themes: RegisteredTheme[] = [];
+  private prefixer: Function = (x => x);
 
   constructor() { }
 
-  registerComponentTheme(componentName: string, componentTheme: CloukitComponentTheme): void {
+  public registerPrefixer(prefixer: Function) {
+    this.prefixer = prefixer;
+  }
+
+  public registerComponentTheme(componentName: string, componentTheme: CloukitComponentTheme): void {
     this.themes.push(new RegisteredTheme(componentName, componentTheme));
   }
 
-  getComponentTheme(componentName: string): CloukitComponentTheme {
+  public getComponentTheme(componentName: string): CloukitComponentTheme {
     const theme = this.themes.filter(registeredTheme => registeredTheme.componentName === componentName);
     if (theme === undefined || theme === null ||
       theme[0] === undefined || theme[0] === null) {
       return null;
     }
     return theme[0].componentTheme;
+  }
+
+  public prefixStyle(style: CloukitStatefulAndModifierAwareElementThemeStyleDefinition): CloukitStatefulAndModifierAwareElementThemeStyleDefinition {
+    style.style = this.prefixer(style.style);
+    return style;
   }
 
 }
